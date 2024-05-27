83bda85ec04bd54bbde5a0a087c1209806606940
Dede <154935734+rhoda-lee@users.noreply.github.com>
2024-05-27 14:20:21 +0000

    final comit

--- rn-assignment-2/App.js
+++ rn-assignment-2/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>My name is 
+        <Text style={[styles.text, styles.boldText]}> Rhoda</Text>
+        </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text:{
+    fontSize: 24,
+  },
+
+  boldText: {
+    fontWeight: 'bold',
+  }
 });
